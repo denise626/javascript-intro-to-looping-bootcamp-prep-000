80cc68c8285522f578332b1cf82e7a9039d38bf9
April Kiser <denise626@hotmail.com>
2018-05-05 01:04:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -14,18 +14,18 @@ function whileLoop(n) {
   let countdown = n;
   
   while (countdown > 0) {
-    console.log(--countdown)
+    console.log(--countdown);
   }
     return 'done';
 }
 
 function doWhileLoop(array) {
   function maybeTrue() {
-    return Math.random() >= 0.5
+    return Math.random() >= 0.5;
   }
     
   do {
     array.pop();
   } while (array.length > -1 && maybeTrue());
-  
+    return array;
 }
